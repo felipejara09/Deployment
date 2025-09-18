e4eec74933d6ffff02b5af713e90655f26d23afd
felipejara09 <felipe.jaramillo0927@gmail.com>
2025-09-17 19:24:45 -0500

    eliminacion de sql inecesarios y agregar conterizacion de aws

--- db/migration/V3__seed_admin.sql
+++
@@ -1,9 +0,0 @@
--- Admin inicial: felipe.jaramillo@crediya.com / contra123*
-INSERT INTO users
-  (name, last_name, email, identity_number, date_born, address, phone_number, id_role, base_salary, password)
-VALUES
-  ('Felipe','Jaramillo','felipe.jaramillo@crediya.com','1012345678','1990-01-01','NA','0000000',1,10000000,
-   crypt('contra123*', gen_salt('bf')))
-ON CONFLICT (email) DO UPDATE
-  SET password = EXCLUDED.password,
-      id_role  = EXCLUDED.id_role;
